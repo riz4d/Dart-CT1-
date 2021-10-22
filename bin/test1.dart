e1f87e9ea32357ab6c1c7a96e2944f8c5e2c74ec
Muhammed Rizad <rizaddq789654@gmail.com>
2021-10-22 08:41:25 -0400

    Add files via upload

---
+++ bin/test1.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+void main() {
+  // print('rizad is here');
+  // var input1 = stdin.readLineSync();
+  // var input2 = stdin.readLineSync();
+  // var num1 = int.parse(input1!);
+  // var num2 = int.parse(input2!);
+  // print('SUM=${num1 + num2}');
+  // print('enter a number');
+  // var input = stdin.readLineSync();
+  // var num = int.parse(input!);
+
+  // if (num < 0) {
+  //   print("it'snegative");
+  // } else if (num == 0) {
+  //   print('number is zero');
+  // } else {
+  //   print("it's positive");
+  // }
+
+  // print('enter a digit');
+  // var input = stdin.readLineSync();
+  // var num = int.parse(input!);
+  // for (var i = 0; i <= num; i++) {
+  //   print(i);
+  // }
+
+  // var numberlist = [12, 35, 36, 10, 23, 45];
+  // print('enter a digit to Add list');
+  // var inputadd = stdin.readLineSync();
+  // var numadd = int.parse(inputadd!);
+  // numberlist.add(numadd);
+  // print(numberlist);
+  // print('enter a digit to search');
+  // var input = stdin.readLineSync();
+  // var num = int.parse(input!);
+  // // print(numberlist.length);
+  // if (numberlist.contains(num)) {
+  //   print('list contain the digit');
+  // } else {
+  //   print("list doesn't contain the digit");
+  // }
+  // print(numberlist.join('*'));
+
+  // print('enter list value.');
+  // var input = stdin.readLineSync();
+  // var num = int.parse(input!);
+  // print('enter index number');
+  // var input1 = stdin.readLineSync();
+  // var num1 = int.parse(input1!);
+  // print("result is");
+  // List<List<int>> list = [
+  //   [1, 3, 2],
+  //   [5, 3, 6]
+  // ];
+  // print(list[num][num1]);
+
+  // Set<int> numberset = {
+  //   1,
+  //   6,
+  //   2,
+  //   3,
+  //   8,
+  //   2,
+  //   3,
+  //   8,
+  // };
+
+  // print(numberset);
+  // var maps = {
+  //   "name": "muhammed rizad",
+  //   "gender": "Male",
+  //   "age": 18,
+  // };
+  // print(maps['name']);
+  // print(maps['gender']);
+  // print(maps['age']);
+String inputname = stdin.readLineSync()
+print("hello, $name");
+}
